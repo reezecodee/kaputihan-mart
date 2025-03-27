9193f0d96ed0d882b1972916f95ee340a9c51f3a
Reeze <azfaalharits25@gmail.com>
2025-03-27 15:36:17 +0700

    feat: add UI auth login

--- start/routes.ts
+++ start/routes.ts
@@ -9,7 +9,14 @@
 
 const HomeController = () => import('#controllers/user/home_controller')
 const DashboardController = () => import('#controllers/admin/dashboard_controller')
+const AuthController = () => import('#controllers/auth/auth_controller')
 import router from '@adonisjs/core/services/router'
 
 router.get('/', [HomeController, 'home'])
 router.get('admin', [DashboardController, 'dashboard'])
+
+router
+  .group(() => {
+    router.get('login', [AuthController, 'login']).as('auth.login')
+  })
+  .prefix('auth')
