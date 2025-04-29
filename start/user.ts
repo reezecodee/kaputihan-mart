427e43890151b7bdb6645bc4573b1dab52b7c46b
Reeze <azfaalharits25@gmail.com>
2025-04-29 21:44:01 +0700

    style: config UI for user page part 2

--- start/user.ts
+++ start/user.ts
@@ -2,7 +2,7 @@ import router from '@adonisjs/core/services/router'
 
 const HomeController = () => import('#controllers/user/home_controller')
 // const CartsController = () => import('#controllers/user/carts_controller')
-// const ProfilesController = () => import('#controllers/user/profiles_controller')
+const ProfilesController = () => import('#controllers/user/profiles_controller')
 const ProductsController = () => import('#controllers/user/products_controller')
 const StoreProfilesController = () => import('#controllers/user/store_profiles_controller')
 // const AboutController = () => import('#controllers/user/about_controller')
@@ -10,6 +10,7 @@ const StoreProfilesController = () => import('#controllers/user/store_profiles_c
 // const HelpsController = () => import('#controllers/user/helps_controller')
 
 router.get('/', [HomeController, 'home']).as('user.home')
+router.get('/profile-saya', [ProfilesController, 'profile']).as('user.profile')
 router.get('daftar-produk', [ProductsController, 'productList']).as('user.productList')
 router.get('detail-produk', [ProductsController, 'productDetail']).as('user.productDetail')
 router.get('profile-toko', [StoreProfilesController, 'storeProfile']).as('user.storeProfile')
