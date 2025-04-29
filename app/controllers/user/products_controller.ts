6188982abb38e5ad977a7a9fde00d3e31bcb63b3
Reeze <azfaalharits25@gmail.com>
2025-04-29 16:27:06 +0700

    style: config UI for user page

--- app/controllers/user/products_controller.ts
+++ app/controllers/user/products_controller.ts
@@ -1,11 +1,19 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ProductsController {
-  public product({ view }: HttpContext) {
+  public productList({ view }: HttpContext) {
+    view.share({
+      title: 'Daftar Produk',
+    })
+
+    return view.render('pages/user/product-list')
+  }
+
+  public productDetail({ view }: HttpContext) {
     view.share({
       title: 'Detail Produk',
     })
 
-    return view.render('pages/user/product')
+    return view.render('pages/user/product-detail')
   }
 }
