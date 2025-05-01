a172ec27099d1c1f6a043e3bf00e98ce96d2476c
Reeze <azfaalharits25@gmail.com>
2025-05-01 16:24:36 +0700

    feat: update help pages

--- app/controllers/user/helps_controller.ts
+++ app/controllers/user/helps_controller.ts
@@ -1,9 +1,17 @@
+import Help from '#models/help'
+import User from '#models/user'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class HelpsController {
-  public help({ view }: HttpContext) {
+  public async help({ view, auth }: HttpContext) {
+    const profile = await User.findOrFail(auth.user?.id)
+
     view.share({
       title: 'Pusat Bantuan',
+      helps: await Help.query()
+        .select(['id', 'judul', 'status', 'created_at'])
+        .where('pengguna_id', profile.id)
+        .limit(10),
     })
 
     return view.render('pages/user/help')
--- start/user.ts
+++ start/user.ts
@@ -1,18 +1,20 @@
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 const HomeController = () => import('#controllers/user/home_controller')
-// const CartsController = () => import('#controllers/user/carts_controller')
 const ProfilesController = () => import('#controllers/user/profiles_controller')
 const ProductsController = () => import('#controllers/user/products_controller')
 const StoreProfilesController = () => import('#controllers/user/store_profiles_controller')
-// const AboutController = () => import('#controllers/user/about_controller')
-// const CategoriesController = () => import('#controllers/user/categories_controller')
 const HelpsController = () => import('#controllers/user/helps_controller')
 
 router.get('/', [HomeController, 'home']).as('user.home')
-router.get('/profile-saya', [ProfilesController, 'profile']).as('user.profile')
 router.get('daftar-produk/:id', [ProductsController, 'productList']).as('user.productList')
 router.get('detail-produk/:slug', [ProductsController, 'productDetail']).as('user.productDetail')
 router.get('profile-toko', [StoreProfilesController, 'storeProfile']).as('user.storeProfile')
-router.get('pusat-bantuan', [HelpsController, 'help']).as('user.help')
-router.get('detail-laporan', [HelpsController, 'helpDetail']).as('user.helpDetail')
+router
+  .group(() => {
+    router.get('/profile-saya', [ProfilesController, 'profile']).as('user.profile')
+    router.get('pusat-bantuan', [HelpsController, 'help']).as('user.help')
+    router.get('detail-laporan', [HelpsController, 'helpDetail']).as('user.helpDetail')
+  })
+  .use(middleware.auth())
